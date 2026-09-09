72f850c24ea652487111e363c21f6175f8e06efa
1Uncs <daemon4treats@gmail.com>
2026-09-09 10:45:53 +0100

    fix: target Android 16 (API 36) and widen support to Android 8+
    - Add expo-build-properties with compile/target SDK 36, minSdk 26
    - Drop stale targetSdkVersion 34 pin from minSdkFix (floor only now)
    - Play requires target API 36 for updates since Aug 31, 2026

--- app.minSdkFix.js
+++ app.minSdkFix.js
@@ -2,10 +2,10 @@ const { withAndroidManifest } = require('expo/config-plugins');
 
 module.exports = function (config) {
   return withAndroidManifest(config, (config) => {
+    // Floor only: target/compile SDK are owned by expo-build-properties.
     config.modResults.manifest['uses-sdk'] = [{
       $: {
-        'android:minSdkVersion': '29',
-        'android:targetSdkVersion': '34'
+        'android:minSdkVersion': '26'
       }
     }];
     return config;
